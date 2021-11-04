1c7b54d836238b2a9606f7d89e634efe95ab0221
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-04 18:09:00 +0000

    🗃 Added database schemas

---
+++ src/schemas/authenticator.schema.ts
@@ -0,0 +1,17 @@
+/* eslint @typescript-eslint/no-explicit-any:0 */
+import { Schema, Document } from 'mongoose';
+
+export interface IAuthenticatorDocument extends Document {
+  [_id: string]: any;
+  credentialID: Buffer;
+  credentialPublicKey: Buffer;
+  counter: number;
+  transports?: AuthenticatorTransport[];
+}
+
+export const authenticatorSchema = new Schema({
+  credentialID: { type: Buffer, required: true },
+  credentialPublicKey: { type: Buffer, required: true },
+  counter: { type: Number, required: true },
+  transports: Array,
+});
