9452c840c5bf5f0de42dbab61346207742bf1165
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-04 18:09:42 +0000

    🔧 Added new .env vars

--- src/configuration/environment.ts
+++ src/configuration/environment.ts
@@ -18,8 +18,31 @@ const environemtSchema = Joi.object({
     .allow('production')
     .allow('test')
     .default('production'),
+  JWT_SECRET: Joi.string()
+    .required()
+    .description('Used to validate a jwt. Use a strong secret!'),
+  JWT_EXPIRESIN: Joi.string()
+    .default('1h')
+    .description('Defines how long a user will be logged in'),
   PORT: Joi.number()
     .default(4040),
+  REGISTRATION_TOKEN_LIFETIME: Joi.string()
+    .default('15m')
+    .description('Defines how long a registration token can be used until it expires'),
+  RP_NAME: Joi.string()
+    .default('Secure Booking Service')
+    .description('Human-readable title of the website for webauthn'),
+  RP_ID: Joi.string()
+    .when('NODE_ENV', {
+      is: Joi.string().equal('development'),
+      then: Joi.string().default('localhost'),
+      otherwise: Joi.string().min(8).required()
+    })
+    .description('Unique identifier of the website for webauthn'),
+  RP_ORIGIN: Joi.string()
+    .uri()
+    .required()
+    .description('Unique identifier of the website for webauthn'),
 }).unknown()
 
 const { error, value: envVars } = environemtSchema.validate(process.env);
@@ -31,5 +54,15 @@ export const config = {
     host: envVars.MONGO_HOST,
     mongooseDebug: envVars.MONGOOSE_DEBUG,
   },
+  jwt: {
+    secret: envVars.JWT_SECRET,
+    expiresIn: envVars.JWT_EXPIRESIN,
+  },
   port: envVars.PORT,
+  registrationTokenLifetime: envVars.REGISTRATION_TOKEN_LIFETIME,
+  rp: {
+    name: envVars.RP_NAME,
+    id: envVars.RP_ID,
+    origin: envVars.RP_ORIGIN,
+  }
 }
