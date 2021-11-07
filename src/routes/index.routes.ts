4d90b9f6bf6c9d8b30bace824a7e7dbfb55d5d84
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-07 15:42:45 +0000

    ⚗ Added routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,2 +1,5 @@
 import { Router } from 'express';
+import { authenticationRoutes } from './authentication.routes';
 export const router = Router();
+
+router.use('/authentication', authenticationRoutes)
