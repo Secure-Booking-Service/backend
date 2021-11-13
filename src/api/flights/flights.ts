1c3a20f2732bb27a1798441db1dcab2a29769b52
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2021-11-13 19:02:46 +0100

    Finish flights api

--- src/api/flights/flights.ts
+++ src/api/flights/flights.ts
@@ -1,12 +1,44 @@
 import { config } from "../../configuration/environment";
-import Amadeus from 'amadeus';
-import { NextFunction, Request, Response } from 'express';
-import { ApiSuccess } from '../success.class';
+import Amadeus from "amadeus";
+import { NextFunction, Request, Response } from "express";
+import { ApiSuccess } from "../success.class";
 import { ApiError } from "../error.class";
+import { loggerFile } from "../../configuration/logger";
+import Joi from "joi";
 
-const amadeus = new Amadeus({
+/**
+ * The base Amadeus api object
+ */
+export const amadeus = new Amadeus({
   clientId: config.amadeusApi.key,
-  clientSecret: config.amadeusApi.secret
+  clientSecret: config.amadeusApi.secret,
+  logger: loggerFile,
+  logLevel: config.env === "development" ? "debug" : "silent",
+});
+
+/**
+ * User input validation
+ */
+const flightsGetRequestSchema = Joi.object({
+  originLocationCode: Joi.string()
+    .required()
+    .length(3)
+    .uppercase()
+    .description("IATA code of the departing airport"),
+  destinationLocationCode: Joi.string()
+    .required()
+    .length(3)
+    .uppercase()
+    .description("IATA code of the destination airport"),
+  departureDate: Joi.date()
+    .required()
+    .iso()
+    .raw()
+    .description("The date on which the traveler will depart"),
+  adults: Joi.number()
+    .required()
+    .min(1)
+    .description("The number of adult travelers"),
 });
 
 /****************************************
@@ -14,24 +46,57 @@ const amadeus = new Amadeus({
  * **************************************/
 
 /**
- * Simple endpoint to verify that the user is authenticated
+ * GET /flights
+ *
+ * Get all flights for the given search criteria.
+ *
+ * Expect: 'originLocationCode', 'destinationLocationCode', 'departureDate' and 'adults' parameter
  *
  * @export
  * @param {Request} req
  * @param {Response} res
  */
-export async function flightsGetRequest(req: Request, res: Response, next: NextFunction) {
+export async function flightsGetRequest(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    const result = await amadeus.shopping.flightOffersSearch.get({
-      originLocationCode: 'SYD',
-      destinationLocationCode: 'BKK',
-      departureDate: '2021-11-15',
-      adults: '2'
+    // 1. Validate user input
+    const assertionGetRequest = flightsGetRequestSchema.validate(req.query);
+    if (assertionGetRequest.error)
+      throw new ApiError(400, assertionGetRequest.error.message);
 
-    })
-    const response = new ApiSuccess(200, result.data);
-    next(response);
-  } catch (error) {
-    throw new ApiError(400, error.message);
+    try {
+      // 2. Call Amadeus API
+      const result = await amadeus.shopping.flightOffersSearch.get({
+        originLocationCode: assertionGetRequest.value.originLocationCode,
+        destinationLocationCode: assertionGetRequest.value.destinationLocationCode,
+        departureDate: assertionGetRequest.value.departureDate,
+        adults: assertionGetRequest.value.adults,
+      });
+
+      // 3. Create return array
+      const flights = result.data.map((flight: any) => {
+        return {
+          id: flight.id,
+          oneWay: flight.oneWay,
+          numberOfBookableSeats: flight.numberOfBookableSeats,
+          stops: flight.itineraries[0].segments.length - 1,
+          price: flight.price.grandTotal,
+          currency: flight.price.currency,
+        };
+      });
+
+      // 4. Done
+      const response = new ApiSuccess(200, flights);
+      next(response);
+    } catch (error) {
+      loggerFile.error(error);
+      throw new ApiError(400, error.code);
+    }
+  } catch (err) {
+    loggerFile.error(err);
+    next(err);
   }
 }
