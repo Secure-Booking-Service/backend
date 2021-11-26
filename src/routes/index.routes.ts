951b0a11c8e187b9fed596a5d568f3ac78933803
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2021-11-26 14:57:24 +0100

    Add role check

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,6 @@
+import { Roles } from '@secure-booking-service/common-types/Roles';
 import { Router } from 'express';
-import { isAuth } from '../api/authentication';
+import { hasRole, isAuth } from '../api/authentication';
 import { flightsGetRequest } from '../api/flights/flights';
 import { authenticationRoutes } from './authentication.routes';
 import { userRoutes } from './user.routes';
@@ -10,4 +11,4 @@ router.use(isAuth);
 
 // EVERYTHING BELOW REQUIRES AUTHENTICATION!
 router.use('/user', userRoutes);
-router.get('/flights', flightsGetRequest);
+router.get('/flights', hasRole(Roles.TRAVELAGENT), flightsGetRequest);
